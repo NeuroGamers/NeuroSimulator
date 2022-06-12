bed72c2f3aba8a0786ab2df537e8c10135811ea4
Muhammad Abdullah Khalid <48885205+makhalid1999@users.noreply.github.com>
2022-06-12 10:49:35 +0500

    Update Interface_With_Agent.cs

--- Interface_With_Agent.cs
+++ Interface_With_Agent.cs
@@ -142,18 +142,4 @@ public class CSharpForGIT : MonoBehaviour
 
         return result;
     }
-    /*
-    public static string GetLocalIPAddress()
-    {
-        var host = Dns.GetHostEntry(Dns.GetHostName());
-        foreach (var ip in host.AddressList)
-        {
-            if (ip.AddressFamily == AddressFamily.InterNetwork)
-            {
-                return ip.ToString();
-            }
-        }
-        throw new System.Exception("No network adapters with an IPv4 address in the system!");
-    }
-    */
 }
